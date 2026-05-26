fb6e377e1da103b9b4946eecda19ad5f2eb34b4c
Steliana Dimitrova <steliana.dimitrova18@gmail.com>
2026-05-27 00:35:15 +0300

    Update component styling

--- src/components/ImageCard.jsx
+++ src/components/ImageCard.jsx
@@ -2,7 +2,7 @@ import "./ImageCard.css";
 
 export default function ImageCard({ src, label, timestamp }) {
   return (
-    <section>
+    <section className="section">
       <img src={src} alt={label} className="card" />
       <p>{timestamp}</p>
     </section>
